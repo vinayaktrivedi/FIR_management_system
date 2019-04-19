cdaeed2324c33f190dc5ceea928784d2a0b83996
paramansh <paramansh9895@gmail.com>
2019-04-19 18:42:07 +0530

    profile db change

---
+++ registerUser.php
@@ -0,0 +1,146 @@
+<?php
+$template = '<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Contact V1</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+
+	<div class="contact1">
+	
+		<div class="container-contact1">	
+			<form action="home.php" method="post">
+			 	<input type="hidden" name="stage" value="backtohome" >	
+			 	<input class="contact1-form-btn" type="submit" name="allreg" style="float:right;" value="Home" >
+			</form>
+			<br>
+			<form action="home.php" method="post">
+			 	<input type="hidden" name="stage" value="logout" >	
+			 	<input class="contact1-form-btn" type="submit" name="allreg" style="float:right;" value="Log out" >
+			</form>
+
+			<form class="contact1-form validate-form" action="registerFir.php" method="post">
+
+			<input type="hidden" name="stage" value="register_submit" >
+				<span class="contact1-form-title">
+					Register USER
+				</span>
+
+				<div class="wrap-input1 validate-input">
+					<input class="input1" type="text" name="id_proof_no" placeholder="ID Proof Number">
+					<span class="shadow-input1"></span>
+				</div>
+				<div class="wrap-input1 validate-input">
+					<input class="input1" type="text" name="id_proof_type" placeholder="ID Proof Type">
+					<span class="shadow-input1"></span>
+				</div>
+				<div class="wrap-input1 validate-input">
+					<input class="input1" type="text" name="location" placeholder="Location">
+					<span class="shadow-input1"></span>
+				</div>
+				<div class="wrap-input1 validate-input">
+					<input class="input1" type="text" name="crime_id" placeholder="Crime ID">
+					<span class="shadow-input1"></span>
+				</div>
+				<div class="wrap-input1 validate-input" >
+					<textarea class="input1" name="description" placeholder="Provide Description"></textarea>
+					<span class="shadow-input1"></span>
+				</div>
+				<div class="container-contact1-form-btn">
+					<button class="contact1-form-btn">
+						<span>
+							Register
+							<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+						</span>
+					</button>
+				</div>
+			</form>
+		</div>
+	</div>
+
+
+
+
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/tilt/tilt.jquery.min.js"></script>
+	<script >
+		$(".js-tilt").tilt({
+			scale: 1.1
+		})
+	</script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag("js", new Date());
+
+  gtag("config", "UA-23581568-13");
+</script>
+
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>';
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+	if (!isset($_SESSION["station_id"])){
+		header('Location: http://localhost:8080');
+	}
+	else{
+		echo $template;
+	}
+}
+#showing form for registering FIR
+else if($_POST["stage"]=="register_form"){
+	if (!isset($_SESSION["station_id"])){
+		header('Location: http://localhost:8080');
+	}
+	else{
+		echo $template;
+	}
+}
+
+else if($_POST["stage"]=="register_submit"){
+	if (!isset($_SESSION["station_id"])){
+		header('Location: http://localhost:8080');
+	}
+	else{
+		$db = new SQLite3('mysqlitedb.db');
+		$name = test_input($_POST["name"]);
+	  	$email = test_input($_POST["email"]);
+	  	$website = test_input($_POST["website"]);
+	  	$comment = test_input($_POST["comment"]);
+	  	$gender = test_input($_POST["gender"]);
+  		$qstr = "insert into records values ('$name', '$email', '$website', '$gender')";
+  		$insres = $db->query($qstr);
+	}
+}
+?>
